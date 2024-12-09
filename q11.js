a591a5224f37ee621d358c86bee0ef5b2e2f51f0
eduarda-sofferreira <espf1@aluno.ifal.edu.br>
2024-12-09 19:41:35 -0300

    Create q11.js

---
+++ q11.js
@@ -0,0 +1,11 @@
+var c = 1
+var maisq8 = 0
+while ( c <= 20) {
+    var numero = parseInt (prompt ("informe um número"))
+    if ( numero > 8){
+        maisq8++;
+    }
+    c++
+}
+
+    console.log(maisq8 + "números são maiores que 8")
